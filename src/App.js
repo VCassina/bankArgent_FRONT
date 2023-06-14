657dcc792e71a5eabb1549f8c7e628a873538f7d
VCassina <victorcassina@gmail.com>
2023-06-14 16:58:08 +0200

    Début de projet.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Routes from "./Routes";
+import { Helmet } from 'react-helmet';
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_div">
+      <Helmet>
+        <title>ArgentBank - Votre banque en ligne</title>
+      </Helmet>
+      <Routes />
     </div>
   );
 }
---
+++ src/pages/homePage.js
@@ -0,0 +1,12 @@
+/* CODE REACT DE LA GENERATION DE PAGE "ACCUEIL" ! */
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="homepage_div">
+        HOMEPAGE
+    </div>
+  );
+}
+
+export default HomePage;
